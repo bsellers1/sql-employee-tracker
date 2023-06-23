9be0883dc3df473624a2be93001ad64e5c5f5a85
Brian Sellers <bsellers3013@gmail.com>
2023-06-22 23:00:17 -0500

    added prompt and .then for start

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 const inquirer = require('inquirer');
 const db = require('./connect');
 
+function start() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'startMenu',
+            message: 'What would you like to do?',
+            choices: [
+                "Add a employee",
+                "Update an employee role",
+                "View all employees",
+                "Add role",
+                "View all roles",
+                "Add a department",
+                "View all departments",
+                "Goodbye",
+            ],
+        },
+    ])
+        .then(function (answer) {
+            switch (answer.startMenu) {
+                case "Add a employee":
+                    addEmployee();
+                    break;
+                case "Update an employee role":
+                    updateRole();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+            }
+        })
+}
